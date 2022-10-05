b8784c6fd5e096eaaee67129fb69ed837df97648
Smirnov Vlad <smirnov.vladislav@phystech.edu>
2022-10-05 14:05:10 +0300

    99% achieved by one_letter_test. TODO: get rid of output-function-coverage

--- prac-1/Finite-automation/tests/one_letter_nfa/test_one_letter_nfa.cpp
+++ prac-1/Finite-automation/tests/one_letter_nfa/test_one_letter_nfa.cpp
@@ -54,23 +54,30 @@ TEST_F(one_letter_NFA_test, is_each_reachable) {
 }
 
 bool one_letter_NFA_test::is_each_finishible(const one_letter_NFA& obj) {
-  std::vector<bool> used(obj._state_cnt + 1, false);
-  std::queue<uint32_t> q;
-  q.push(obj._start_id);
-  while (!q.empty()) {
-    uint32_t state_id = q.front();
-    q.pop();
-    used[state_id] = true;
-    for (auto& [str, state_set]: obj._gr[state_id]) {
-      for (uint32_t cur_state_id: state_set) {
-        if (!used[cur_state_id]) {
-          q.push(cur_state_id);
+  // one dfs-call is more efficiently, but currently there is enough dfs functions... :)
+  // mb to do
+  for (uint32_t state_id = 1; state_id <= obj._state_cnt; ++state_id) {
+    bool is_finishible = obj._is_finish[state_id];
+    std::vector<bool> used(obj._state_cnt + 1, false);
+    std::queue<uint32_t> q;
+    q.push(state_id);
+    while (!q.empty()) {
+      uint32_t front_state = q.front();
+      q.pop();
+      used[front_state] = true;
+      for (auto& [str, state_set]: obj._gr[front_state]) {
+        for (uint32_t cur_state_id: state_set) {
+          if (!used[cur_state_id]) {
+            if (obj._is_finish[cur_state_id]) {
+              is_finishible = true;
+              break;
+            }
+            q.push(cur_state_id);
+          }
         }
       }
     }
-  }
-  for (uint32_t state_id = 1; state_id <= obj._state_cnt; ++state_id) {
-    if (!obj._gr[state_id].empty() && !used[state_id]) {
+    if (!is_finishible) {
       return false;
     }
   }
@@ -82,6 +89,15 @@ TEST_F(one_letter_NFA_test, is_each_finishible) {
     NFA& nfa = *ptr;
     one_letter_NFA one_letter_nfa(nfa);
     EXPECT_EQ(is_each_finishible(one_letter_nfa), true);
+  }
+}
+
+TEST_F(one_letter_NFA_test, cover_show_function) {
+  for (auto ptr: data) {
+    NFA& nfa = *ptr;
+    one_letter_NFA one_letter_nfa(nfa);
+    one_letter_nfa.show_finit_automaton("foo.doa");
+    EXPECT_EQ(false, false);
   }
 }
 
